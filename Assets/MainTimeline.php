c770b49e609639e2b4979993b4cd90d673c40b0d
RickBeniers <Beniersrick@gmail.com>
2022-10-27 00:47:44 +0200

    completed timelind generation fuction and work around for page width limitation. 27-10-22, 0047

--- Assets/MainTimeline.php
+++ Assets/MainTimeline.php
@@ -5,28 +5,52 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-    <link rel="stylesheet" href="MainTmelineStyle.css?v=012" type="text/css">
+    <link rel="stylesheet" href="MainTmelineStyle.css?v=020" type="text/css">
     <?php include 'MainTimelineFunctions.php'?>
 </head>
-<body>
+<body id="body">
 <header id="UpperContent"></header>
 <div id="CentreContent">
     <div id="TimelineInfoDisplay">
         <div class="outerInfoDisplayBox">
             <div class="innerInfoDisplayBox">
                 <div class="widthInfoDisplayBox" >
-                    <div class="headOfStateTitleDiv titleDivSpecifics"></div>
-                    <div class="headOfGovermentTitleDiv titleDivSpecifics"></div>
+                    <div class="headOfStateTitleDiv titleDivSpecifics">
+                        <div class="headOfStateImgDiv"><img ></div>
+                        <div class="headOfStateTitleDisplay">
+                            <h4 class="titleTekstPosition">firstname + lastname</h4>
+                            <h4 class="titleTekstPosition">title</h4>
+                            <h4 class="titleTekstPosition">party affiliation</h4>
+                        </div>
+                    </div>
+                    <div class="headOfGovermentTitleDiv titleDivSpecifics">
+                        <div class="headOfGovermentImgDiv"><img ></div>
+                        <div class="headOfGovermentTitleDisplay">
+                            <h4 class="titleTekstPosition">firstname + lastname</h4>
+                            <h4 class="titleTekstPosition">title</h4>
+                            <h4 class="titleTekstPosition">party affiliation</h4>
+                        </div>
+                    </div>
                 </div>
                 <div class="widthInfoDisplayBox">
-                    <div class="congressChamberSpeakerTitleDiv"></div>
+                    <div class="congressChamberSpeakerTitleDiv leftChamber01SpeakerTitleDiv">
+                        <h4 class="speakerTitleDisplay">Title + name</h4>
+                    </div>
+                    <div class="congressChamberSpeakerTitleDiv rightChamber01SpeakerTitleDiv">
+                        <h4 class="speakerTitleDisplay"></h4>
+                    </div>
                     <div class="chamber01SeatDistributionHolder">
                         <div class="congressSeatDistributionDiv leftChamber01SeatDistributionDiv"></div>
                         <div class="congressSeatDistributionDiv rightChamber01SeatDistributionDiv"></div>
                     </div>
                 </div>
                 <div class="widthInfoDisplayBox">
-                    <div class="congressChamberSpeakerTitleDiv"></div>
+                    <div class="congressChamberSpeakerTitleDiv leftChamber02SpeakerTitleDiv">
+                        <h4 class="speakerTitleDisplay">Title + name</h4>
+                    </div>
+                    <div class="congressChamberSpeakerTitleDiv rightChamber02SpeakerTitleDiv">
+                        <h4 class="speakerTitleDisplay"></h4>
+                    </div>
                     <div class="chamber02SeatDistributionHolder">
                         <div class="congressSeatDistributionDiv leftChamber02SeatDistributionDiv"></div>
                         <div class="congressSeatDistributionDiv rightChamber02SeatDistributionDiv"></div>
@@ -36,8 +60,10 @@
         </div>
     </div>
     <div id="TimelineBox">
-        <div class="TimelineRulerHor"></div>
-        <?php automatedTimelineCreator(8, 2000); ?>
+        <div class="TimelineRulerHor" id="HorizontalLine"></div>
+        <div class="timeLineYearsInLine">
+            <?php automatedTimelineCreator(30, 2000); ?>
+        </div>
     </div>
 </div>
 <footer id="LowerContent"></footer>
--- Assets/MainTimelineFunctions.php
+++ Assets/MainTimelineFunctions.php
@@ -1,9 +1,10 @@
 <?php
 function automatedTimelineCreator($totalYearsPar, $StartYearPar){
     $StartYear = $StartYearPar;
+    echo '<script>var lineWidth=0;</script>';
     for($i = 1; $i <= $totalYearsPar; $i++){
-        $StartYear++;
         //create HTML elements for 1 years worth
+        echo '<script>lineWidth=lineWidth+100;</script>';
         echo '<div class="OneYearTimelineBlock">
                 <div class="TimelineVerSticks YearIndication YearIndicatorPosition"></div>
                 <div class="TimelineVerSticks SeasonsIndication" style="margin-left: 22.5px"></div>
@@ -12,11 +13,43 @@ function automatedTimelineCreator($totalYearsPar, $StartYearPar){
                 <h4 class="YearTextIndicator">'; print($StartYear);
         echo '</h4>
             </div>';
+        $StartYear++;
+        echo '<script>document.getElementById("HorizontalLine").style.width=lineWidth+"px";
+        //console.log(document.getElementById("HorizontalLine").style.width)</script>';
     }
 }
 function automatedInfoBoxCreator($totalTimeSpanPar){
     for($i = 1; $i <= $totalTimeSpanPar; $i++){
+        echo'<div class="widthInfoDisplayBox" >
+                    <div class="headOfStateTitleDiv titleDivSpecifics">
 
+                    </div>
+                    <div class="headOfGovermentTitleDiv titleDivSpecifics"></div>
+                </div>
+                <div class="widthInfoDisplayBox">
+                    <div class="congressChamberSpeakerTitleDiv leftChamber01SpeakerTitleDiv">
+                        <h4 class="speakerTitleDisplay">Title + name</h4>
+                    </div>
+                    <div class="congressChamberSpeakerTitleDiv rightChamber01SpeakerTitleDiv">
+                        <h4 class="speakerTitleDisplay">Title + name</h4>
+                    </div>
+                    <div class="chamber01SeatDistributionHolder">
+                        <div class="congressSeatDistributionDiv leftChamber01SeatDistributionDiv"></div>
+                        <div class="congressSeatDistributionDiv rightChamber01SeatDistributionDiv"></div>
+                    </div>
+                </div>
+                <div class="widthInfoDisplayBox">
+                    <div class="congressChamberSpeakerTitleDiv leftChamber02SpeakerTitleDiv">
+                        <h4 class="speakerTitleDisplay">Title + name</h4>
+                    </div>
+                    <div class="congressChamberSpeakerTitleDiv rightChamber02SpeakerTitleDiv">
+                        <h4 class="speakerTitleDisplay">Title + name</h4>
+                    </div>
+                    <div class="chamber02SeatDistributionHolder">
+                        <div class="congressSeatDistributionDiv leftChamber02SeatDistributionDiv"></div>
+                        <div class="congressSeatDistributionDiv rightChamber02SeatDistributionDiv"></div>
+                    </div>
+                </div>';
     }
 }
 ?>
