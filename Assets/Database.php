1e50ee40a4cf5fed517b1107eadf8943d35a19d2
RickBeniers <Beniersrick@gmail.com>
2022-11-03 19:16:44 +0100

    created database connection and ERD. 03-11-22 1916

---
+++ Assets/Database.php
@@ -0,0 +1,26 @@
+<?php
+function ConnectToDatabase()
+{
+    $host = "127.0.0.1";
+    $port = 3306;
+    $user = "root";
+    $password = "Rick3109";
+    $dbname = "PoliticalHistoryDb";
+
+    $connection = null;
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    try {
+        $connection = new mysqli($host, $user, $password, $dbname, $port);
+        mysqli_set_charset($connection, 'latin1');
+        $DatabaseAvailable = true;
+    }catch (mysqli_sql_exception $e) {
+        $DatabaseAvailable = false;
+    }
+    if(!$DatabaseAvailable) {
+        echo '<h4>E001 - Database not available. Tip: restart database engine.</h4>';
+        die();
+    }
+    return $connection;
+    //$con->close();
+}
+?>
--- Assets/MainTimelineFunctions.php
+++ Assets/MainTimelineFunctions.php
@@ -71,4 +71,3 @@ function automatedInfoBoxCreator($totalTerms){
     }
 }
 ?>
-
